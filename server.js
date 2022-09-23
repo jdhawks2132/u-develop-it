eaa354a33443e71f12939fb31b6e5f8bfeab42d9
Joshua Hawks <jdhawks@gmail.com>
2022-09-23 07:08:33 -0500

    connect node to database

--- server.js
+++ server.js
@@ -1,9 +1,21 @@
 const express = require('express');
+const mysql = require('mysql2');
 const PORT = process.env.PORT || 3000;
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+// Connect to database
+const db = mysql.createConnection(
+	{
+		host: 'localhost',
+		// MySQL username,
+		user: 'root',
+		database: 'election',
+	},
+	console.log('Connected to the election database.')
+);
 
 app.listen(PORT, () => {
 	console.log(`App listening on PORT: ${PORT}`);
